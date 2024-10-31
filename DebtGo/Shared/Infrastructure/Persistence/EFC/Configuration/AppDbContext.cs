ac465252a0d2662b282618a7753d215e79be7025
Franco Durand <francoduve.25@gmail.com>
2024-10-30 22:20:48 -0500

    feat: implement user authentication endpoints

--- DebtGo/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ DebtGo/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,5 @@
+using DebtGo.IAM.Domain.Model.Aggregates;
+using DebtGo.IAM.Domain.Model.Entities;
 using DebtGo.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +19,21 @@ namespace DebtGo.Shared.Infrastructure.Persistence.EFC.Configuration
             base.OnModelCreating(modelBuilder);
 
             //TODO agregar tablas    
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+            modelBuilder.Entity<User>().Property(u => u.Name).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Email).IsRequired();
+
+            modelBuilder.Entity<PaymentCard>().HasKey(p => p.Id);
+            modelBuilder.Entity<PaymentCard>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            modelBuilder.Entity<PaymentCard>().Property(p => p.CardNumber).IsRequired();
+            modelBuilder.Entity<PaymentCard>().Property(p => p.ExpiryDate).IsRequired();
+            modelBuilder.Entity<PaymentCard>().Property(p => p.CVV).IsRequired().HasColumnName("cvv");
+            modelBuilder.Entity<PaymentCard>()
+                .HasOne(p => p.User)
+                .WithOne(u => u.PaymentCard)
+                .HasForeignKey<PaymentCard>(p => p.UserId);
 
             modelBuilder.UseSnakeCaseNamingConvention();
         }
