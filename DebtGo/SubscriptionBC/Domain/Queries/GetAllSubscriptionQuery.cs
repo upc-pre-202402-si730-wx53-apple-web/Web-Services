cfaa29e247dbcc352799a33acb08954936976b7a
Daniel-10806 <U202211108@upc.edu.pe>
2024-11-11 19:12:54 -0500

    feat(domain): add GetAllSubscriptionQuery

---
+++ DebtGo/SubscriptionBC/Domain/Queries/GetAllSubscriptionQuery.cs
@@ -0,0 +1,10 @@
+namespace DebtGo2.SubscriptionBC.Domain.Model.Queries
+{
+    /// <summary>
+    ///     Represents a query to retrieve all subscriptions.
+    /// </summary>
+    public class GetAllSubscriptionsQuery
+    {
+        // No additional properties required.
+    }
+}
